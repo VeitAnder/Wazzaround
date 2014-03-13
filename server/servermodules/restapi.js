e00df5bb6b4a6124599f71887897dd3545ac73eb
Maximilian Schmid <ms@anorak.io>
2014-03-13 18:25:23 +0100

    + dropzone upload works + upload/delete on cloudinary works + dropzone css styles integrated

--- server/servermodules/restapi.js
+++ server/servermodules/restapi.js
@@ -1,7 +1,22 @@
 var upload = require('../routes/upload.js');
 
 var RestApi = function (app) {
-  app.post('/upload', upload.postupload);
+  // filter to allow cross origin requests
+  app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+//    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+    if ('OPTIONS' === req.method) {
+      return res.send(200);
+    }
+    next();
+  });
+
+  // Cloudinary Upload routes
+  app.post('/upload/activityimage/', upload.postupload);
+  app.delete('/upload/activityimage/:resourceid/', upload.deleteupload);
 };
 
 module.exports = {
