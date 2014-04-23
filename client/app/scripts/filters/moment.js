b39dd892fe23efbb4482aa5756e40db1a6d40533
Maximilian Schmid <ms@anorak.io>
2014-04-23 17:10:18 +0200

    + edit activity event - start end date now works

--- client/app/scripts/filters/moment.js
+++ client/app/scripts/filters/moment.js
@@ -1,12 +1,16 @@
+"use strict";
 angular.module('momentjs', []).
-  filter('fromNow',function () {
-    "use strict";
+  filter('fromNow', function () {
     return function (dateString) {
       return moment(dateString).fromNow();
     };
   })
+  .filter('minusOneDay', function () {
+    return function (dateString) {
+      return moment(dateString).subtract('days', 1).toDate();
+    };
+  })
   .filter('momentjs', function ($translate) {
-    "use strict";
     return function (data, config) {
       moment.lang($translate.use());
       return moment(data).format(config.format);
