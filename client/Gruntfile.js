d0cd6486bac1e94eed5b8fc4a711a156e72337a2
Maximilian Schmid <ms@anorak.io>
2014-04-30 10:56:44 +0200

    + fixed image problem on workwithuspage, update grunt build process

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -188,7 +188,7 @@ module.exports = function (grunt) {
       // concat, minify and revision files. Creates configurations in memory so
       // additional tasks can operate on them
       useminPrepare: {
-        html: ['<%= yeoman.app %>/index.html', '<%= yeoman.app %>/views/**/*.html'],
+        html: ['<%= yeoman.app %>/index.html'],
         options: {
           dest: '<%= yeoman.dist %>'
         }
