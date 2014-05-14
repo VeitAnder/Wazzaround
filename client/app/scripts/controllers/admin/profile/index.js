91f1cc7284a104105d5c18014cdb225812a9acb3
Maximilian Schmid <ms@anorak.io>
2014-05-14 16:45:31 +0200

    + profile edit page - restored form check functionality

--- client/app/scripts/controllers/admin/profile/index.js
+++ client/app/scripts/controllers/admin/profile/index.js
@@ -21,9 +21,13 @@ angular.module('anorakApp')
     };
 
     $scope.user = currentUser.user;
-    $scope.state = {};
+
+    $scope.state = {
+      submitted: false
+    };
 
     $scope.saveUserProfile = function () {
+      $scope.state.submitted = true;
 
       if ($scope.valForm.$valid) {
         $scope.user.save()
@@ -39,6 +43,21 @@ angular.module('anorakApp')
           });
 
       }
+    };
+
+    /**
+     * Whether to show an error message for the specified error
+     * @param {string} fieldName The name of the field on the form, of which we want to know whether to show the error
+     * @param  {string} error - The name of the error as given by a validation directive
+     * @return {Boolean} true if the error should be shown
+     */
+    $scope.showError = function (fieldName, error) {
+      var formName = "valForm";
+      var showerror = false;
+      if ($scope[formName][fieldName].$error[error] && (!$scope[formName][fieldName].$pristine || $scope.state.submitted)) {
+        showerror = true;
+      }
+      return showerror;
     };
 
     $scope.Countrylist = Countrylist;
