5eb3c51907fd44516ed1fee92554b745ca949552
Maximilian Schmid <ms@anorak.io>
2014-06-02 10:15:38 +0200

    Activitybar: no image renders emtpy img-tag #154 - hide empty img tag

--- client/app/scripts/directives/cloudinaryimage.js
+++ client/app/scripts/directives/cloudinaryimage.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('anorakApp')
-  .directive('cloudinaryimage', function (APP_CONFIG) {
+  .directive('cloudinaryimage', function (APP_CONFIG, $timeout) {
     return {
       template: '<img ng-show="publicid" ng-src="{{getCloudinaryurl()}}" /> ',
       restrict: 'E',
@@ -15,7 +15,16 @@ angular.module('anorakApp')
       link: function postLink(scope, element, attrs) {
         $.cloudinary.config({ cloud_name: APP_CONFIG.cloudinary.cloud_name, api_key: APP_CONFIG.cloudinary.api_key});
         scope.getCloudinaryurl = function () {
-          return $.cloudinary.url(scope.publicid + "." + scope.format, { width: scope.width, height: scope.height, crop: 'fill' });
+          var url;
+          if (scope.publicid !== "") {
+            url = $.cloudinary.url(scope.publicid + "." + scope.format, { width: scope.width, height: scope.height, crop: 'fill' });
+          } else {
+            console.log("scope.publicid", scope.publicid === "");
+            scope.getCloudinaryurl = function () {
+              url = "#";
+            };
+          }
+          return url;
         };
       }
     };
