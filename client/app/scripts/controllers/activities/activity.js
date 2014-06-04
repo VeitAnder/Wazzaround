a66e8729dd59abc7856a8998b671a6978a4dfb8c
Maximilian Schmid <ms@anorak.io>
2014-06-04 14:27:54 +0200

    + correct marker icon in activity detail view

--- client/app/scripts/controllers/activities/activity.js
+++ client/app/scripts/controllers/activities/activity.js
@@ -25,6 +25,10 @@ angular.module('anorakApp')
       }
     };
 
+    $scope.getMarkerIcon = function () {
+      return "/img/mapicons/marker-"+ activity.category.main+".png";
+    };
+
 //    $scope.models.UserModel.getProfile({id:$scope.activity.owner._reference})
 //      .then(function(res) {
 //        $scope.$apply(function() {
