b04250fb58009c9051c652fcd25f3c461a90de61
Jonathan Häberle <jonathan.haeberle@gmail.com>
2014-06-14 15:00:44 +0200

    filter selection ui implemented

--- client/app/scripts/directives/bookableitemlist.js
+++ client/app/scripts/directives/bookableitemlist.js
@@ -10,42 +10,15 @@ angular.module('anorakApp')
         showpagination: '@showpagination',
         eventlimit: '@limit'
       },
-      compile: function compile(tElement, tAttrs, transclude) {
-        return {
-          pre: function preLink(scope, element, attrs, controller) {
+      controller : function($scope) {
+        console.log("scope.limit", $scope.limit);
 
-            console.log("scope.limit", scope.limit);
+        if (!$scope.eventlimit) {
+          $scope.eventlimit = 10000000000;
+        }
 
-            if (!scope.eventlimit) {
-              scope.eventlimit = 10000000000;
-            }
-
-            scope.state = {
-              limit: scope.eventlimit
-            };
-
-            scope.addToShoppingCart = function (event, bookableItem, activity) {
-              // $timeout ensures cart update in view
-              $timeout(function () {
-                shoppingcart.add({
-                  eventId: event._id,
-                  bookableItemId: bookableItem._id,
-                  activityId: activity._id,
-                  description: bookableItem.description,
-                  price: event.price,
-                  quantity: 1,
-                  start: event.start,
-                  end: event.end,
-                  category: activity.category.main,
-                  image: activity.images[0]
-                });
-              });
-            };
-
-          },
-          post: function postLink(scope, iElement, iAttrs, controller) {
-
-          }
+        $scope.state = {
+          limit: $scope.eventlimit
         };
 
       }
