e3deaf0b0f6bdcf3a448049ad898e47faf6e8598
Jonathan Häberle <jonathan.haeberle@gmail.com>
2014-06-17 15:06:56 +0200

    sorted events by date

--- client/app/scripts/directives/bookableitemlist.js
+++ client/app/scripts/directives/bookableitemlist.js
@@ -5,17 +5,51 @@ angular.module('anorakApp')
     return {
       templateUrl: 'views/directives/bookableitemlist.html',
       restrict: 'E',
-      controller : function($scope) {
+      scope: {
+        activity: '=',
+        filter: '=',
+        limit: '='
+      },
+      controller: function ($scope) {
 
-        $scope.isItemEnabled = function(item) {
+        var isItemEnabled = function (item) {
+          if (!$scope.filter) return true;  // nicht filtern, wenn kein filter definiert ist
+
           return item.filter.enabled();
         };
 
-        $scope.isEventEnabled = function(event) {
+        var isEventEnabled = function (event) {
+          if (!$scope.filter) return true;  // nicht filtern, wenn kein filter definiert ist
+
           if (event.start < $scope.filter.from) return false;
           if (event.start > $scope.filter.until) return false;
           return true;
         };
+
+        $scope.getSortedEvents = function () {
+          // erst mal filtern
+          var sortedEvents = [];
+
+          _.forEach($scope.activity.bookableItems, function (bookableItem) {
+            if (isItemEnabled(bookableItem)) {
+              _.forEach(bookableItem.events, function (event) {
+                if (isEventEnabled(event)) {
+                  sortedEvents.push({
+                    date: event.start,
+                    activity: $scope.activity,
+                    bookableItem: bookableItem,
+                    event: event
+                  });
+                }
+              });
+            }
+          });
+
+          // dann nach datum sortieren
+          sortedEvents = _.sortBy(sortedEvents, 'date');
+
+          return sortedEvents;
+        }
       }
     };
   });
