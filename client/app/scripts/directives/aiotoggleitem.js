603945a4e3303be5b822cf2c00d2cd4ea5ab60da
Maximilian Schmid <ms@anorak.io>
2014-07-28 14:17:42 +0200

    Event Duration #216

--- client/app/scripts/directives/aiotoggleitem.js
+++ client/app/scripts/directives/aiotoggleitem.js
@@ -1,24 +1,20 @@
 'use strict';
 
 angular.module('anorakApp')
-  .directive('aiotoggleitem', function () {
+  .directive('aiotoggleitem', function ($translate) {
     return {
       template: '<div ng-class="{open: state.open}">' +
-        '<a ng-click="state.toggle()" class="aiotoggleitemhead"><span>{{state.toggleitemlabel}}</span><i>?</i></a> ' +
+        '<a ng-click="state.toggle()" class="aiotoggleitemhead"><span>{{toggleitemlabel}}</span><i>?</i></a> ' +
         '<div ng-transclude ng-show="state.open" class="aiotoggleitembody"></div> ' +
         '</div>',
       restrict: 'A',
       transclude: true,
       scope: {
+        toggleitemlabel: "@"
       },
       link: function postLink(scope, element, attrs) {
-        if (!attrs.toggleitemlabel) {
-          attrs.toggleitemlabel = "toggle";
-        }
-
         scope.state = {
           open: false,
-          toggleitemlabel: attrs.toggleitemlabel,
           toggle: function () {
             this.open = !this.open;
           }
--- client/app/scripts/directives/datetimepicker.js
+++ client/app/scripts/directives/datetimepicker.js
@@ -15,68 +15,41 @@ angular.module('anorakApp')
           },
           post: function postLink(scope, iElement, iAttrs, controller) {
 
-            function setTimeToNextFiveMinuteInterval(dateTime) {
-              var time = moment(dateTime);
-              var restmin = time.minute() % 15;
-              var addmin = 0;
-              if (restmin > 0) {
-                addmin = 15 - restmin;
-                time.minute(time.minute() + addmin);
-              }
-              return time.toDate();
-            }
+            // get days, hours and minutes from end time
+            var duration = moment.duration(moment(scope.event.end) - moment(scope.event.start));
+            scope.event.durationdays = duration.days();
+            scope.event.durationhours = duration.hours();
+            scope.event.durationminutes = duration.minutes();
 
-            // start ist nach end - setze end auf start + 1h
-            scope.event.start = setTimeToNextFiveMinuteInterval(scope.event.start);
-            scope.event.end = setTimeToNextFiveMinuteInterval(scope.event.end);
-
-            scope.setEndDate = function () {
-              if (moment(scope.event.start).isAfter(moment(scope.event.end))) {
-                $timeout(function () {
-                  // don't assign new Object, instead alter existing date object
-                  scope.event.end = moment(moment(scope.event.start)).add('hours', 1).toDate();
-                });
-              }
+            var getEndDate = function () {
+              var endDate = new moment(scope.event.start);
+              endDate.add('days', scope.event.durationdays);
+              endDate.add('hours', scope.event.durationhours);
+              endDate.add('minutes', scope.event.durationminutes);
+              return endDate;
             };
 
-            scope.$watch('event.start', function (newDate, oldDate) {
-              if (newDate !== oldDate) {
-                scope.setEndDate();
+            var updateEndDate = function () {
+              scope.event.end = getEndDate().toDate();
+            };
+
+            scope.$watch('event.durationdays', function (newVal, oldVal) {
+              if (newVal !== oldVal) {
+                updateEndDate();
               }
             }, true);
 
-            scope.$watch('event.end', function (newDate, oldDate) {
-              if (newDate !== oldDate) {
-                var start = moment(scope.event.start);
-                var end = moment(scope.event.end);
-
-                if (start.isAfter(end)) {
-                  // don't assign new Object, instead alter existing date object
-                  $timeout(function () {
-                    scope.event.end = start.add(15, 'minutes').toDate();
-                  });
-                }
-
+            scope.$watch('event.durationhours', function (newVal, oldVal) {
+              if (newVal !== oldVal) {
+                updateEndDate();
               }
             }, true);
 
-            scope.getMinTimeForEndTime = function () {
-              // @TODO check is wrong ---
-              if (moment(scope.event.start).diff(moment(scope.event.end), 'days') > -1) {
-                // same day - apply hours rules
-                var hours = moment(scope.event.start).hours();
-                var minutes = moment(scope.event.start).minutes();
-                if (hours < 10) {
-                  hours = "0" + hours;
-                }
-                if (minutes < 10) {
-                  minutes = "0" + minutes;
-                }
-                return hours + ":" + minutes;
-              } else {
-                return "00:00";
+            scope.$watch('event.durationminutes', function (newVal, oldVal) {
+              if (newVal !== oldVal) {
+                updateEndDate();
               }
-            };
+            }, true);
 
           }
         };
