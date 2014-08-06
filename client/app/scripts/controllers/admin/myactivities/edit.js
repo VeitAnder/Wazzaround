8344800ad52598e4ff8752681666dcb576d1c89b
Maximilian Schmid <ms@anorak.io>
2014-08-06 15:34:10 +0200

    + acitvity form now with ng-messages, formcheck error messages fixed and now complete

--- client/app/scripts/controllers/admin/myactivities/edit.js
+++ client/app/scripts/controllers/admin/myactivities/edit.js
@@ -346,21 +346,6 @@ angular.module('anorakApp')
 
     };
 
-    /**
-     * Whether to show an error message for the specified error
-     * @param {string} fieldName The name of the field on the form, of which we want to know whether to show the error
-     * @param  {string} error - The name of the error as given by a validation directive
-     * @return {Boolean} true if the error should be shown
-     */
-    $scope.showError = function (fieldName, error) {
-      var formName = "valForm";
-      var showerror = false;
-      if ($scope[formName][fieldName].$error[error] && (!$scope[formName][fieldName].$pristine || $scope.state.submitted)) {
-        showerror = true;
-      }
-      return showerror;
-    };
-
     $scope.additionalFormChecks = function () {
       var valid = true;
 
