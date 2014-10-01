19c30fdf981cd5375b1e288f986bd014a9ba94a8
Jonathan Häberle <jonathan.haeberle@gmail.com>
2014-10-01 15:53:48 +0200

    added filter for all activities

--- client/app/scripts/controllers/adminactivityfilterbar.js
+++ client/app/scripts/controllers/adminactivityfilterbar.js
@@ -8,11 +8,51 @@
  * Controller of the anorakApp
  */
 angular.module('anorakApp')
-  .controller('AdminactivityfilterbarCtrl', function ($scope, $filter) {
-    var self = this;
-    $scope.$watch(function () {
-      return self.query;
-    }, function () {
-      $scope.vm.filteredActivities = $filter('filter')($scope.activities, self.query);
+    .controller('AdminactivityfilterbarCtrl', function ($scope, $filter) {
+
+      var self = this;
+
+      this.query = '';
+
+      this.filters = {'published': false, 'denied': false, 'unreviewedChanges': false};
+      this.filtersState = {'published': true, 'denied': true, 'unreviewedChanges': true};
+
+      $scope.$watch(function (scope) {
+        return self;
+
+      }, function () {
+        var filteredActivities = $scope.activities;
+
+        filteredActivities = $filter('filter')($scope.activities, self.query);
+
+        if (self.filters.published) {
+          filteredActivities = $filter('filter')(filteredActivities, {
+            published: self.filtersState.published
+          });
+        }
+
+
+        if (self.filters.denied) {
+          filteredActivities = $filter('filter')(filteredActivities, {
+            denied: self.filtersState.denied
+          });
+        }
+
+
+        if (self.filters.unreviewedChanges) {
+          if (self.filtersState.unreviewedChanges) {
+            filteredActivities = $filter('filter')(filteredActivities, {
+              unreviewedChanges: '!0'
+            });
+          } else {
+            filteredActivities = $filter('filter')(filteredActivities, {
+              unreviewedChanges: 0
+            });
+          }
+        }
+
+
+        $scope.vm.filteredActivities = filteredActivities;
+      }, true);
+
     });
-  });
