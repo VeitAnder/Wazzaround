eff3ca6f5ba4d5c660cc9d2ef39ddcd821e315fd
Maximilian Schmid <ms@anorak.io>
2014-10-03 19:23:00 +0200

    don't remove config_developmentmodulus.js via git hooks §

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,20 +1,2 @@
+#!/bin/sh
 # pre-commit git hook to prevent accidential commit of confidential production server data to git repo
-
-FILE=server/config_productionserver.js
-if [ -f $FILE ];
-then
-  echo "File $FILE exists. It will be removed because it contains sensitive data!"
-  rm $FILE
-else
-  echo "File $FILE does not exist. OK. Git commit can continue."
-fi
-
-
-FILE=server/config_developmentmodulus.js
-if [ -f $FILE ];
-then
-  echo "File $FILE exists. It will be removed because it contains sensitive data!"
-  rm $FILE
-else
-  echo "File $FILE does not exist. OK. Git commit can continue."
-fi
---
+++ server/config_developmentmodulus.js
@@ -0,0 +1,61 @@
+var config = {
+  host: "http://dev.reacture.anorak.io/",
+  clienthost: "http://dev.reacture.anorak.io/",
+  mongo: {
+    username: 'reactureapppdevonmodulus',
+    password: 'T88XXE987987cfkKfits',
+    dbName: 'reactureappdevonmodulus',                                // The name of database that contains the security information
+    host: 'ds033679.mongolab.com:33679'                         // mongolab mongodb connection url
+  },
+  security: {
+    usersCollection: 'users',                                   // The name of the collection contains user information
+    passwordencryptionalgorithm: 'aes256',                      // db password encryption algorithm
+    passwordencryptionkey: "EdJkKl$§$&-kjlk$ioi23a4t9T7"        // password encryptionkey
+  },
+  amazons3: {
+//    S3_ACCESS_KEY: 'AKIAJVNTGQMMDTGWNI4A',
+//    S3_SECRET_KEY: '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',
+//    S3_BUCKET_NAME: 'planfredlocalhost',                             // bucket name and bucket URL have to match
+//    S3_BUCKET_URL: 'https://planfredlocalhost.s3.amazonaws.com',
+//    S3_REGION: "eu-west-1"
+  },
+  server: {
+    listenPort: 3000,                                           // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
+    securePort: 8433,                                           // The HTTPS port on which the server is to listen (means that the app is at https://localhost:8433 for instance)
+    distFolder: __dirname + '/clientapp/',                               // The folder that contains the application files (note that the files are in a different repository) - relative to this file
+    cookieSecret: 'scMafaus3dfs4po03CrDAFd7fd45'                // The secret for encrypting the cookie
+  },
+  postmark: {
+    apikey: "b1af3ea7-b17b-493c-a62c-53d3370a3077",              // postmark api key
+    from: "admin@reacture.com",                                // postmark signed from email addresss
+    replyto: "donotreply@reacture.com"
+  },
+  api: {
+    apiversion: "api/v1/"                                       // don't forget trailing slash
+  },
+  monitoring: {
+    enabled: false,
+    updateInterval: 1000,                                      // update custom monitor statistics every 1 seconds
+    profile: {
+      accountKey: '',
+      appName: 'Development Application',
+      debug: false
+    }
+  },
+  logging: {
+    using_logentries_service: false,
+    logentries_token: '',
+    level: 'silly',
+    colorize: true
+  },
+  cloudinary: {
+    cloud_name: 'dqe7zmb1k',
+    api_key: '619226866778758',
+    api_secret: 'yA_Xv9LXc9aEab05M9YNlEgpQsw'
+  },
+  google: {
+    apikey: "AIzaSyDeA3pWT15QSri8b00AVVqVCmAY_Niqbvg"
+  }
+};
+
+module.exports = config;
