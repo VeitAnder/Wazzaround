162c97c34bc3fbda93de92a79cee6479b74769d0
Maximilian Schmid <ms@anorak.io>
2015-06-08 14:39:16 +0200

    deploy_heroku_production.sh

--- deploy_heroku_production.sh
+++ deploy_heroku_production.sh
@@ -3,9 +3,9 @@
 echo "heroku - logout any active login"
 heroku auth:logout
 
-echo "Login as planfredapp@gmail.com:"
+echo "Login as reactureapp@gmail.com:"
 heroku auth:login
 
-git push heroku_appplanfredcom +HEAD:master
+git push heroku_wwwwazzaroundcom +HEAD:master
 
 heroku auth:logout
