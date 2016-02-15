e6dd79d164f7ee25f883d8794726c72b1a2ca467
Maximilian Schmid <ms@anorak.io>
2016-02-15 17:19:50 +0100

    2.4 Preisanzeige in Activity um Gruppen-Events erweitern - https://app.asana.com/0/16075887606903/90333663482903

---
+++ client/app/scripts/directives/event-item-pricing-view.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('anorakApp')
+  .directive('eventItemPricingView', function eventItemPricingViewDirective() {
+
+    var controller = function () {
+      var vm = this;
+    };
+
+    var directiveDefinitionObject = {
+      restrict: 'E',
+      scope: {
+        event: "="
+      },
+      controller: controller,
+      controllerAs: 'ctrl',
+      bindToController: true, //required in 1.3+ with controllerAs
+      templateUrl: 'views/directives/event-item-pricing-view.html'
+    };
+    return directiveDefinitionObject;
+
+  });
